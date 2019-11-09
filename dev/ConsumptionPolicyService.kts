0b08c91faf09ea99b4816d08c3322a1f0492f90c
mpeterss <mape@comoyo.com>
2019-11-09 12:04:10 +0100

    Adding whitelist to Digi Develop

--- dev/ConsumptionPolicyService.kts
+++ dev/ConsumptionPolicyService.kts
@@ -2,6 +2,7 @@ import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
 import org.ostelco.ocs.api.MultipleServiceCreditControl
+import org.ostelco.prime.getLogger
 import org.ostelco.prime.ocs.core.ConsumptionPolicy
 import org.ostelco.prime.ocs.core.ConsumptionRequest
 import org.ostelco.prime.storage.ConsumptionResult
@@ -10,6 +11,44 @@ private data class ServiceIdRatingGroup(
         val serviceId: Long,
         val ratingGroup: Long
 )
+
+private val logger by getLogger()
+
+private val digiAllowedMcc = setOf(
+        Mcc.AUSTRALIA.value,
+        Mcc.CHINA.value,
+        Mcc.HONG_KONG.value,
+        Mcc.INDONESIA.value,
+        Mcc.JAPAN.value,
+        Mcc.MALAYSIA.value,
+        Mcc.NORWAY.value,
+        Mcc.PHILIPPINES.value,
+        Mcc.SINGAPORE.value,
+        Mcc.THAILAND.value,
+        Mcc.SOUTH_KOREA.value,
+        Mcc.VIET_NAM.value
+)
+
+enum class Mcc(val value: String) {
+    AUSTRALIA("505"),
+    CHINA("460"),
+    HONG_KONG("454"),
+    INDONESIA("510"),
+    JAPAN("440"),
+    MALAYSIA("502"),
+    NORWAY("242"),
+    PHILIPPINES("515"),
+    THAILAND("520"),
+    SINGAPORE("525"),
+    SOUTH_KOREA("450"),
+    VIET_NAM("452")
+}
+
+enum class MccMnc(val value: String) {
+    M1("52503"),
+    DIGI("50216"),
+    LOLTEL("24201")
+}
 
 object : ConsumptionPolicy {
 
@@ -24,6 +63,7 @@ object : ConsumptionPolicy {
         val used = multipleServiceCreditControl.used?.totalOctets ?: 0
 
         if (!isMccMncAllowed(sgsnMccMnc, imsiMccMnc)) {
+            logger.warn("Blocked usage for sgsnMccMnc $sgsnMccMnc imsiMccMnc $imsiMccMnc msisdn $msisdn ")
             return blockConsumption(msisdn)
         }
 
@@ -48,9 +88,9 @@ object : ConsumptionPolicy {
             ServiceIdRatingGroup(402L, 402L),       // TATA
             ServiceIdRatingGroup(409L, 409L)        /* Digi */ -> {
                 ConsumptionResult(
-                    msisdnAnalyticsId = msisdn,
-                    granted = multipleServiceCreditControl.requested.totalOctets,
-                    balance = multipleServiceCreditControl.requested.totalOctets * 100
+                        msisdnAnalyticsId = msisdn,
+                        granted = multipleServiceCreditControl.requested.totalOctets,
+                        balance = multipleServiceCreditControl.requested.totalOctets * 100
                 ).left()
             }
 
@@ -68,10 +108,11 @@ object : ConsumptionPolicy {
     }
 
     fun isMccMncAllowed(sgsnMccMnc: String, imsiMccMnc: String) : Boolean {
+        val sgsnMcc = sgsnMccMnc.substring(range = 0..2)
         return when (imsiMccMnc) {
-            "52503" -> true  // M1
-            "50216" -> true  // Digi
-            "24201" -> true  // Telenor Norway
+            MccMnc.M1.value -> true
+            MccMnc.DIGI.value -> digiAllowedMcc.contains(sgsnMcc)
+            MccMnc.LOLTEL.value -> true
             else -> false
         }
     }
